795d111ce755ac9f4665ed60c60627701ed9ffa3
Yannick PEROUX <yannick.peroux@gmail.com>
2018-12-10 19:28:06 +0100

    Fix export.sh

--- export.sh
+++ export.sh
@@ -2,5 +2,5 @@
 
 # the server must run (npm start) at localhost:8000
 
-docker pull astefanutti/decktape
-docker run --rm --net=host -v `pwd`:/slides astefanutti/decktape -s 1366x768 http://localhost:8000 slides.pdf
+docker pull astefanutti/decktape:2.8.5
+docker run --rm --net=host -v `pwd`:/slides astefanutti/decktape:2.8.5 -s 1920x1080 http://localhost:8000 slides.pdf
